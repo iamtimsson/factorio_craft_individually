3f087f350244750eb3324fcdbf99a85695bbd38e
Jason Fortit <json.commits@gmail.com>
2022-07-26 21:57:04 +0800

    Initial commit

---
+++ control.lua
@@ -0,0 +1,8 @@
+script.on_event('individual-craft-5', function(event)
+    if event.selected_prototype.base_type == "recipe" then
+        local plr = game.players[event.player_index]
+        for i = 1, 5, 1 do
+            plr.begin_crafting{count=1, recipe=event.selected_prototype.name, silent=true}
+        end
+    end
+end)
---
+++ data.lua
@@ -0,0 +1,12 @@
+data:extend(
+        {
+            {
+                type = "custom-input",
+                name = "individual-craft-5",
+                key_sequence = "mouse-button-2",
+                linked_game_control = "craft-5",
+                consuming = "game-only",
+                include_selected_prototype = true
+            }
+        }
+)
