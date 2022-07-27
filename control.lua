159a26f79fdfc1b8a498d8a984586637b213e49e
Jason Fortit <json.commits@gmail.com>
2022-07-27 20:42:15 +0800

    Fixed a bug where crafting 5 items would index a null value.

--- control.lua
+++ control.lua
@@ -1,8 +1,11 @@
 script.on_event('individual-craft-5', function(event)
-    if event.selected_prototype.base_type == "recipe" then
-        local plr = game.players[event.player_index]
-        for i = 1, 5, 1 do
-            plr.begin_crafting{count=1, recipe=event.selected_prototype.name, silent=true}
+    local selected_prototype = event.selected_prototype
+    if selected_prototype then
+        if selected_prototype.base_type == "recipe" then
+            local plr = game.players[event.player_index]
+            for _ =1, 5, 1 do
+                plr.begin_crafting{count=1, recipe=selected_prototype.name, silent=true}
+            end
         end
     end
 end)
